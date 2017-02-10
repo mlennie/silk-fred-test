ee38f442ec30fc8efc0abc764dd5e310954ac545
Monty Lennie <montylennie@gmail.com>
2017-02-10 22:55:09 +0000

    delete temp files after finished

--- app/models/line.rb
+++ app/models/line.rb
@@ -8,14 +8,18 @@ class Line < ApplicationRecord
 
   def create_montage total_lines
 
+    temp_path_one ='./temp_img_one_' + id.to_s + '.png'
+    temp_path_two ='./temp_img_two_' + id.to_s + '.png'
+    temp_path_three = "temp_" + id.to_s + ".png"
+
     ########################
     # Get Images
     ########################
 
     download = open(url_one)
-    IO.copy_stream(download, './temp_img_one_' + id.to_s + '.png')
+    IO.copy_stream(download, temp_path_one)
     download = open(url_two)
-    IO.copy_stream(download, './temp_img_two_' + id.to_s + '.png')
+    IO.copy_stream(download, temp_path_two)
 
     ########################
     # Create file and combine images
@@ -24,20 +28,20 @@ class Line < ApplicationRecord
     new_montage = ImageList.new
     row = ImageList.new
 
-    row.push(Image.read('./temp_img_one_' + id.to_s + '.png').first)
-    row.push(Image.read('./temp_img_two_' + id.to_s + '.png').first)
+    row.push(Image.read(temp_path_one).first)
+    row.push(Image.read(temp_path_one).first)
 
     new_montage.push (row.append(false))
 
     picture = new_montage.flatten_images
-    file = Tempfile.new("temp_" + id.to_s + ".png")
+    file = Tempfile.new(temp_path_three)
 
     picture.write(file.path)
 
     ########################
     # Add watercolor
     ########################
-    img = Magick::Image.read("temp_" + id.to_s + ".png").first
+    img = Magick::Image.read(temp_path_three).first
     mark = Magick::Image.read("./generic_logo.png").first
     img1 = img.composite(mark, Magick::CenterGravity, Magick::SoftLightCompositeOp)
     img1.write(file.path)
@@ -48,6 +52,10 @@ class Line < ApplicationRecord
 
     file.close
     file.unlink
+
+    File.delete(temp_path_one)
+    File.delete(temp_path_two)
+
 
     self.document.update!(complete: true) if document.lines.count == total_lines
 
