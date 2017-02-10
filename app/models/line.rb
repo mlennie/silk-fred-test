6d53eeab116ba2d465cd0d78fdeb839df9c9c386
Monty Lennie <montylennie@gmail.com>
2017-02-10 19:56:33 +0000

    make combined image

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,8 +1,21 @@
+require 'rmagick'
+
 class Line < ApplicationRecord
+
+include Magick
 
   belongs_to :document
 
   def create_montage total_lines
+    montage = ImageList.new
+    row = ImageList.new
+
+    row.push(Image.read(url_one).first)
+    row.push(Image.read(url_two).first)
+
+    montage.push (row.append(false))
+    montage.append(true).write("montage_" + id + ".jpg")
+
     document.update(complete: true) if document.lines.count == total_lines
   end
 
