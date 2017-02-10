31bf52701b9979c3bec86828c7945e9985370174
Monty Lennie <montylennie@gmail.com>
2017-02-10 22:14:21 +0000

    add host_url env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  Rails.application.routes.default_url_options[:host] = ENV[:host]
+  Rails.application.routes.default_url_options[:host] = ENV["HOST_URL"]
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
