1ffe892dd597fc016355507fabb7b3ee78a73559
Monty Lennie <montylennie@gmail.com>
2017-02-10 21:50:13 +0000

    add heroku gems

--- app/models/line.rb
+++ app/models/line.rb
@@ -9,25 +9,29 @@ class Line < ApplicationRecord
   def create_montage total_lines
 
     download = open(url_one)
-    IO.copy_stream(download, './temp_img_one.png')
+    IO.copy_stream(download, './temp_img_one_' + id.to_s + '.png')
     download = open(url_two)
-    IO.copy_stream(download, './temp_img_two.png')
+    IO.copy_stream(download, './temp_img_two_' + id.to_s + '.png')
 
     new_montage = ImageList.new
     row = ImageList.new
 
-    row.push(Image.read("./temp_img_one.png").first)
-    row.push(Image.read("./temp_img_two.png").first)
+    row.push(Image.read('./temp_img_one_' + id.to_s + '.png').first)
+    row.push(Image.read('./temp_img_two_' + id.to_s + '.png').first)
 
     new_montage.push (row.append(false))
 
     picture = new_montage.flatten_images
-    file = Tempfile.new("temp_image.jpg")
+    file = Tempfile.new("temp_" + id.to_s + ".jpg")
     picture.write(file.path)
 
-    self.update(montage:file.read)
+    data = file.read
+    self.update!(montage:data)
+    file.close
+    file.unlink
 
-    document.update(complete: true) if document.lines.count == total_lines
+    self.document.update!(complete: true) if document.lines.count == total_lines
+    return true
   end
 
 end
