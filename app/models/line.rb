6437cc83366bb89d274fdcabb0e4a9b1753bfb7b
Monty Lennie <montylennie@gmail.com>
2017-02-10 22:50:04 +0000

    remove temp images and start adding watercolor

--- app/models/line.rb
+++ app/models/line.rb
@@ -8,10 +8,18 @@ class Line < ApplicationRecord
 
   def create_montage total_lines
 
+    ########################
+    # Get Images
+    ########################
+
     download = open(url_one)
     IO.copy_stream(download, './temp_img_one_' + id.to_s + '.png')
     download = open(url_two)
     IO.copy_stream(download, './temp_img_two_' + id.to_s + '.png')
+
+    ########################
+    # Create file and combine images
+    ########################
 
     new_montage = ImageList.new
     row = ImageList.new
@@ -22,16 +30,27 @@ class Line < ApplicationRecord
     new_montage.push (row.append(false))
 
     picture = new_montage.flatten_images
-    file = Tempfile.new("temp_" + id.to_s + ".jpg")
+    file = Tempfile.new("temp_" + id.to_s + ".png")
+
     picture.write(file.path)
 
+    ########################
+    # Add watercolor
+    ########################
+    img = Magick::Image.read("temp_" + id.to_s + ".png").first
+    mark = Magick::Image.read("./generic_logo.png").first
+    img1 = img.composite(mark, Magick::CenterGravity, Magick::SoftLightCompositeOp)
+    img1.write(file.path)
+
     data = file.read
+
     self.update!(montage:data)
+
     file.close
     file.unlink
 
     self.document.update!(complete: true) if document.lines.count == total_lines
-    return true
+
   end
 
 end
